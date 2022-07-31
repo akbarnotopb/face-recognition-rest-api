ac5105b2080f7607d441b8b12eef3b506ae26803
akbarnotopb <akbar.exp57@gmail.com>
2022-07-31 19:52:29 +0700

    init features

---
+++ src/facedetector/facedetector.py
@@ -0,0 +1,43 @@
+import face_recognition as fr
+import numpy as np
+
+class FaceDetector:
+    def __init__(self):
+        pass
+
+    def compareEmbedding(self, source, target, sourcetype="ndarray" , tolerance = 0.5):
+        databases = []
+        for encoded in source:
+            if(sourcetype == "string"):
+                databases.append(self.string2ndArray(encoded))
+            elif(sourcetype == "list"):
+                databases.append(np.array(encoded))
+            else:
+                databases.append(encoded)
+
+        res = fr.compare_faces(databases, target, tolerance=tolerance)
+        return res
+
+    def extractFeatures(self, file, output="ndarray"):
+        image = fr.load_image_file(file)
+        encoding = fr.face_encodings(image)
+        if(len(encoding) == 0 ):
+            raise Exception("No face detected!")
+        
+        if output == "string":
+            return self.ndArray2String(encoding[0])
+        elif output == "list":
+            return encoding[0].tolist()
+        else:
+            return encoding[0]
+
+    def getFaceCount(self, file):
+        pass
+
+    def ndArray2String(self, ndarray):
+        return np.array2string(ndarray)
+    
+    def string2ndArray(self, string):
+        return np.fromstring(string)
+
+    
---
+++ src/main.py
@@ -0,0 +1,82 @@
+import falcon,json
+from facedetector.facedetector import FaceDetector
+from falcon_multipart.middleware import MultipartMiddleware
+
+MODEL = FaceDetector()
+
+class RegisterFaces:
+    def on_post(self, req, res):
+        incoming_files = req.get_param_as_list("faces")
+        features = []
+        for incoming_file in incoming_files:
+            try:
+                features.append(MODEL.extractFeatures(incoming_file.file,  output="list"))
+            except:
+                features.append("")
+        
+        res.status = falcon.HTTP_200
+        res.text = json.dumps(features)
+
+class RegisterFace:
+    def on_post(self, req, res):
+        incoming_file = req.get_param("face")
+        try:
+            features = MODEL.extractFeatures(incoming_file.file, output="list")
+        except Exception as e:
+            res.status = falcon.HTTP_422
+            res.text = json.dumps({"message":str(e)})
+            return 
+
+        res.status = falcon.HTTP_200
+        res.text = json.dumps(features)
+
+
+
+class VerifyFace:
+    def on_post(self, req, res):
+        incoming_file = req.get_param("face")
+        sourcetype = req.get_param("sourcetype")
+        list_of_features = req.get_param_as_list("encodings") if sourcetype == "string" else json.loads(req.get_param("encodings"))
+        tolerance = req.get_param("tolerance")
+
+        if(tolerance != None):
+            tolerance = float(tolerance)
+
+        features = None
+        extracted = False
+        try:
+            features = MODEL.extractFeatures(incoming_file.file)
+            extracted = True
+        except Exception as e:
+            res.status = falcon.HTTP_422
+            res.text = json.dumps({"message":str(e)})
+            return 
+        
+        if not extracted:
+            res.status = falcon.HTTP_422
+            res.text = json.dumps({"message":"Something went wrong!"})
+            return 
+        
+        verification_result = MODEL.compareEmbedding(list_of_features, features, sourcetype=sourcetype, tolerance = tolerance if tolerance != None else 0.5) 
+
+        res.text = json.dumps(verification_result, default=str)
+
+
+class ServerStatus:
+    def on_get(self, req, res):
+        res.status = falcon.HTTP_200
+        res.text = json.dumps({"message":"running normal"})
+
+
+app = falcon.App(cors_enable = True,middleware=[MultipartMiddleware()])
+
+
+def generic_error_handler(ex, req, resp, params):
+    raise falcon.HTTPInternalServerError(title="500 Internal Server Error", description=str(ex)) 
+
+app.add_error_handler(Exception, generic_error_handler)
+
+app.add_route("/register", RegisterFaces())
+app.add_route("/register-single", RegisterFace())
+app.add_route("/verify", VerifyFace())
+app.add_route("/status", ServerStatus())
