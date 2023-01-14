29e33ab6833b6d4ac6b9173d30427e3aa98ce50b
akbarnotopb <akbar.exp57@gmail.com>
2023-01-15 06:11:40 +0700

    add web registration

--- src/main.py
+++ src/main.py
@@ -4,8 +4,12 @@ from falcon_multipart.middleware import MultipartMiddleware
 import hashlib
 from datetime import datetime
 import os, time
+import logging
+logger = logging.getLogger()
+logger.setLevel("INFO")
 
 SECRET_KEY = ""
+WEB_TOKEN = ""
 with open(".env") as env:
     _str = env.readlines()
     for s in _str:
@@ -13,6 +17,10 @@ with open(".env") as env:
         res = s.split("=")
         if(res[0] == "SECRET_KEY"):
             SECRET_KEY = res[1]
+        elif(res[0] == "WEB_TOKEN"):
+            WEB_TOKEN = res[1]
+
+logger.info(WEB_TOKEN)
 
 MODEL = FaceDetector()
 
@@ -36,7 +44,7 @@ class RegisterFaces:
                     f.write(incoming_file.file.read())
                 features.append(MODEL.extractFeatures(file_path,  output="list", mode="native" if mode == None else mode))
             except:
-                features.append("")
+                features.append("[]")
         
         res.status = falcon.HTTP_200
         res.text = json.dumps(features)
@@ -152,6 +160,30 @@ class VerifyFaceV2:
 
         res.text = json.dumps(verification_result, default=str)
 
+class WebRegistration:
+    def on_post(self, req, res):
+        token = req.get_param("token")
+        sha = req.get_param("sha")
+        mode = req.get_param("mode")
+        
+        if(token == None or sha == None or sha != hashlib.sha256((WEB_TOKEN+token).encode()).hexdigest()):
+            res.status = falcon.HTTP_401
+            res.text = json.dumps({"message":"invalid access!"})
+            return 
+
+        incoming_files = req.get_param_as_list("faces")
+        features = []
+        for incoming_file in incoming_files:
+            try:
+                file_path = "{}/img/register/{}-{}".format( os.getcwd() ,datetime.now().strftime("%Y%m%d-%H:%M:%S-%f"), incoming_file.filename)
+                with open(file_path, "wb") as f:
+                    f.write(incoming_file.file.read())
+                features.append(MODEL.extractFeatures(file_path,  output="list", mode="native" if mode == None else mode))
+            except:
+                features.append("[]")
+        
+        res.status = falcon.HTTP_200
+        res.text = json.dumps(features)
 
 class ServerStatus:
     def on_get(self, req, res):
@@ -168,6 +200,7 @@ def generic_error_handler(ex, req, resp, params):
 app.add_error_handler(Exception, generic_error_handler)
 
 app.add_route("/register", RegisterFaces())
+app.add_route("/web-register", WebRegistration())
 app.add_route("/register-single", RegisterFace())
 app.add_route("/verify", VerifyFace())
 app.add_route("/v2/verify", VerifyFaceV2())
